547654862fe7f9ad6f3bba1f7a8984d0db0726fd
szymonobara <Szymon2904@gmail.com>
2020-12-12 15:39:52 +0000

    a

--- lab6.cpp
+++ lab6.cpp
@@ -1,6 +1,25 @@
+#include "make_random_vector.hpp"
 #include <iostream>
 
 int main()
 {
-    puts("Hello, World!");
+    // int x;
+    // std::cin >> x;
+    // std::vector< int > v;
+    // v = make_random_vector(10, 0, 10);
+    // std::sort(v.begin(), v.end(), [&](int a, int b) { return a > b; });
+    // for (int i = 0; i < 10; i++) {
+    //     std::cout << v[i] << "\n";
+    // }
+    // std::cout << "\n" << std::count_if(v.begin(), v.end(), [&](int a) { return a > x; });
+
+    std::string x;
+    std::cin >> x;
+    auto is = std::adjacent_find(x.begin(), x.end());
+    if (is == x.end()) {
+        std::cout << "0";
+    }
+    else {
+        std::cout << "1";
+    }
 }
--- zal/foo.hpp
+++ zal/foo.hpp
@@ -9,8 +9,10 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
-
-    // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(), [&](Human h) { h.birthday(); });
+    std::transform(people.rbegin(), people.rend(), ret_v.begin(), [&](Human h) {
+        return h.isMonster() ? 'n' : 'y';
+    });
 
     return ret_v;
 }
